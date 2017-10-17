014e1040fcaa8839c96a3c93be3ceba0d3ae69d7
David E. McGruder <mcgruderlaw@gmail.com>
2017-10-16 22:22:23 -0400

    Update qutebrowser config.py

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -1,3 +1,40 @@
-c.tabs.position = "bottom"
-c.url.start_pages = ["https://www.qutebrowser.org/"]
-c.url.start_pages.append("https://www.python.org/")
+c.tabs.position = "top"
+c.url.start_pages = ["about:blank"]
+# c.bindings.default = {"J": "tab-prev", "K": "tab-next"}
+# config.unbind('J', 'tab-next')
+# config.unbind('K', 'tab-prev')
+# config.bind('J', 'tab-prev')
+# config.bind('K', 'tab-next')
+c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}",
+                       "4": "https://4chan.org/{}",
+                       "4cat": "https://4chan.org/{}/catalog",
+                       "8": "https://8ch.net/{}",
+                       "8cat": "https://8ch.net/{}/catalog.html",
+                       "am": "https://www.amazon.com/?search={}",
+                       "aw": "https://wiki.archlinux.org/?search={}",
+                       "eb": "http://ebay.com/sch/{}",
+                       "gh": "https://github.com/search?utf8=/&q={}&type=",
+                       "gw": "https://wiki.gentoo.org/?search={}",
+                       "i": "https://duckduckgo.com/?q=!ddgi {}",
+                       "ig": "https://wiki.installgentoo.com/?search={}",
+                       "im": "http://www.imdb.com/find?s=all&q={}",
+                       "lain": "http://lainchan.org/{}",
+                       "laincat": "http://lainchan.org/{}/catalog.html",
+                       "map": "http://openstreetmap.org/search?query={}",
+                       "r": "https://reddit.com/r/{}",
+                       "tpb": "http://thepiratebay.org/search/{}",
+                       "tw": "https://twitter.com/{}",
+                       "w": "https://en.wikipedia.org/?search={}",
+                       "yt": "https://youtube.com/results?search_query={}"
+                       }
+c.aliases = {"bar": "open https://blackagendareport.com",
+             "bl": "open http://thebiglead.com",
+             "cr": "open http://www.cracked.com",
+             "ds": "open https://deadspin.com",
+             "es": "open http://www.espn.com",
+             "ic": "open https://www.icle.org",
+             "lh": "open https://lifehacker.com",
+             "ra": "open http://www.racer.com",
+             "rm": "open https://rm5.rocketmatter.net/mcgruderlaw/app.aspx#/dashboard",
+             "ro": "open https://www.theroot.com"
+             }
