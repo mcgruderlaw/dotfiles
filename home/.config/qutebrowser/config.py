6faa424ae592ba32faaf9447190e32c15d3c8a02
David E. McGruder <mcgruderlaw@gmail.com>
2017-10-17 16:26:41 -0400

    Additions to qutebrowser config

--- home/.config/qutebrowser/config.py
+++ home/.config/qutebrowser/config.py
@@ -1,10 +1,15 @@
 c.tabs.position = "top"
 c.url.start_pages = ["about:blank"]
-# c.bindings.default = {"J": "tab-prev", "K": "tab-next"}
-# config.unbind('J', 'tab-next')
-# config.unbind('K', 'tab-prev')
-# config.bind('J', 'tab-prev')
-# config.bind('K', 'tab-next')
+config.unbind('J', mode='normal')
+config.unbind('K', mode='normal')
+config.bind('J', 'tab-prev')
+config.bind('K', 'tab-next')
+config.bind('q', 'quit')
+c.fonts.hints = "bold 12pt monospace"
+c.fonts.statusbar = "10 pt monospace"
+c.fonts.tabs = "10pt monospace"
+c.messages.timeout = 7000
+c.hints.uppercase = True
 c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}",
                        "4": "https://4chan.org/{}",
                        "4cat": "https://4chan.org/{}/catalog",
@@ -12,7 +17,6 @@ c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}",
                        "8cat": "https://8ch.net/{}/catalog.html",
                        "am": "https://www.amazon.com/?search={}",
                        "aw": "https://wiki.archlinux.org/?search={}",
-                       "bb": "https://bitbucket.org/search?q={}",
                        "eb": "http://ebay.com/sch/{}",
                        "gh": "https://github.com/search?utf8=/&q={}&type=",
                        "gw": "https://wiki.gentoo.org/?search={}",
@@ -29,12 +33,15 @@ c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}",
                        "yt": "https://youtube.com/results?search_query={}"
                        }
 c.aliases = {"bar": "open https://blackagendareport.com",
+             "bb": "https://bitbucket.org",
              "bl": "open http://thebiglead.com",
              "cr": "open http://www.cracked.com",
              "ds": "open https://deadspin.com",
              "es": "open http://www.espn.com",
+             "git": "open https://www.github.com/mcgruderlaw",
              "ic": "open https://www.icle.org",
              "lh": "open https://lifehacker.com",
+             "michbar": "open https://www.zeekbeek.com/SBM",
              "ra": "open http://www.racer.com",
              "rm": "open https://rm5.rocketmatter.net/mcgruderlaw/app.aspx#/dashboard",
              "ro": "open https://www.theroot.com"
